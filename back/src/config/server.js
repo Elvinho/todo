7eaa6c21e6790d30b9101559cbd38f9f2248c22e
Elvinho <elvison.victor@liferay.com>
2024-01-30 07:44:12 -0300

    criacao do server e iniciando api

--- back/src/config/server.js
+++ back/src/config/server.js
@@ -7,6 +7,8 @@ const server = express();
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
-server.listen(port, () => {
-    console.log(`Backend is runnig on port ${port}.`);
+server.listen(port, function(){
+    console.log(`Backend is runnig on port  http://localhost:${port}.`);
 })
+
+module.exports = server;
